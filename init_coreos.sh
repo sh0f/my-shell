1108ba2e1d11568de76420665b9754110e8126a8
sh0f <30991096+sh0f@users.noreply.github.com>
2018-09-04 09:39:48 +0800

    add tcp_bbr sysctl

--- init_coreos.sh
+++ init_coreos.sh
@@ -4,7 +4,7 @@ update_engine_client -update
 [ -n "`grep ^REBOOT_STRATEGY /etc/coreos/update.conf`" ] && sed -i "s@^REBOOT_STRATEGY.*@REBOOT_STRATEGY=off@" /etc/coreos/update.conf || echo 'REBOOT_STRATEGY=off' >> /etc/coreos/update.conf
 systemctl restart update-engine
 [ ! -d /opt/bin ] && mkdir -p /opt/bin
-wget -O /opt/bin/docker-compose https://github.com/docker/compose/releases/download/1.19.0/docker-compose-Linux-x86_64
+wget -O /opt/bin/docker-compose https://github.com/docker/compose/releases/download/1.22.0/docker-compose-Linux-x86_64
 cat > /opt/bin/lrzsz << EOF
 #!/bin/bash
 docker run -ti --rm -v /root/upload:/upload sh0f/lrzsz
@@ -59,5 +59,8 @@ systemctl enable update-window.timer
 systemctl enable docker
 systemctl enable disable-transparent-huge-pages
 echo 'vm.overcommit_memory=1' > /etc/sysctl.d/overcommit.conf
+echo 'tcp_bbr' > /etc/modules-load.d/bbr.conf
+echo -e 'net.core.default_qdisc=fq\nnet.ipv4.tcp_congestion_control=bbr' > /etc/sysctl.d/bbr.conf
+echo 'net.ipv4.tcp_fastopen=3' > /etc/sysctl.d/fastopen.conf
 [ -z "`grep ^SystemMaxUse /etc/systemd/journald.conf`" ] && echo 'SystemMaxUse=10M' >> /etc/systemd/journald.conf || sed -i "s@^SystemMaxUse.*@SystemMaxUse=10M@" /etc/systemd/journald.conf
 [ ! -d /root/upload ] && mkdir /root/upload
